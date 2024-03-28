30362e750f047b28b39b6d2c4b659ac3780578c6
Andrew Diaz <andrewdiazn20@gmail.com>
2024-03-28 16:38:58 -0700

    3 commmit

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,11 +1,14 @@
-from flask import (BLueprint, render_templates)
+from flask import (Blueprint, render_template)
 import json
 
-pets = json.load(open('pets.json'))
-print(pets)
+pets = json.load(open('./pets.json'))
 
-bp = Blueprint('pet', __name__,url_prefix='/pets')
+bp = Blueprint('pet', __name__, url_prefix='/pets')
 
 @bp.route('/')
 def index():
-    return render_templates('index.html')
+	return render_template('/pets/index.html', pets=pets)
+
+@bp.route('/<int:id>')
+def show_pet(id):
+	return render_template('/pets/show_pet.html', pet=next(filter(lambda p: p['pet_id'] == id, pets)))
